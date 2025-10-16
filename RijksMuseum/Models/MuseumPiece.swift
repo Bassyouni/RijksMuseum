4b2f2b6759e97cd1d7eb5192fe97ae158c15e61f
Bassyouni <omae.bassyouni991@gmail.com>
2025-10-17 00:37:12 +0200

    add domain model MuseumPiece

---
+++ RijksMuseum/Models/MuseumPiece.swift
@@ -0,0 +1,29 @@
+//
+//  MuseumPiece.swift
+//  RijksMuseum
+//
+//  Created by Dionne Hallegraeff on 17/10/2025.
+//
+
+import Foundation
+
+struct MuseumPiece: Equatable, Identifiable {
+    let id: String
+    let title: String
+    let date: String?
+    let creator: String?
+    let image: ResizableImage?
+}
+
+struct ResizableImage: Equatable {
+    private let url: URL?
+
+    init(url: URL?) {
+        self.url = url
+    }
+    
+    func imageURL(width: Double, height: Double) -> URL? {
+        // TODO: add logic
+        return url
+    }
+}
